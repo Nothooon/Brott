c41443aaa75d55372db5ff124ad1732570a36db2
Nothooon <lemoannathan@gmail.com>
2023-03-05 12:31:53 +0100

    Basic Bot structure: main file and poetry files

---
+++ bot/bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('DISCORD_TOKEN'))
