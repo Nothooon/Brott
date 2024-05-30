dc83eea37a6743960648bf710f81830498d03666
Nothon :) <lemoannathan@gmail.com>
2024-05-30 20:53:18 +0200

    feat: detect twitter links, delete original message and send formatted link

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from discord import app_commands
 
 from features import feur, logging
+from features.link_fix import service
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,21 +12,25 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 logger = logging.Logger()
+link_fixer = service.LinkFixService()
 
-@tree.command(name = "leaderboard", description = "Display the leaderboard")
+
+@tree.command(name="leaderboard", description="Display the leaderboard")
 async def leaderboard(ctx):
     res = logger.get_leaderboard()
     await ctx.response.send_message(embed=res)
 
-@tree.command(name = "feur", description = "Display the user's feur count")
+
+@tree.command(name="feur", description="Display the user's feur count")
 async def feur_user(ctx, user: discord.Member = None):
-    if user == None:
+    if user is None:
         user = ctx.user
     if user.bot:
         await ctx.response.send_message("A bot can't be feur'd!", ephemeral=True)
     else:
         res = logger.get_user(user.name)
         await ctx.response.send_message(embed=res)
+
 
 @client.event
 async def on_ready():
@@ -35,18 +40,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user: # Useless? bot seems to ignore other bots (including itself?)
+    if message.author == client.user:  # Useless? bot seems to ignore other bots (including itself?)
         return
+
+    if link_fixer.detect_inner_links(message.content):
+        await message.channel.send(link_fixer.handle_message(message))
 
     # The one and only necessary feature
     if "quoi" in message.content.lower():
         feur_controller = feur.FeurController(message)
         answer, sticker = feur_controller.create_feur_answer()
-        if answer or sticker:  # Depending on our luck, we don't answer "feur" and the controller returns an empty message
+        if answer or sticker:  # Depending on our luck, we don't answer "feur" but use a sticker instead
             logger.log_feur(message.author.name)
             if sticker:
                 await message.channel.send(file=sticker)
             else:
                 await message.channel.send(content=answer)
+
 
 client.run(os.getenv('DISCORD_TOKEN'))
---
+++ features/link_fix/service.py
@@ -0,0 +1,65 @@
+import json
+import re
+import discord
+
+
+class LinkInfos:
+    def __init__(self, original_link: str, app_name: str, post_id: str) -> None:
+        self.original_link = original_link
+        self.app_name = app_name
+        self.post_id = post_id
+
+
+class LinkFixService:
+
+    def __init__(self):
+        with open("config/config.json") as config_file:
+            self.config = json.load(config_file)
+
+    def handle_message(self, message: discord.Message) -> None:
+        if not message:
+            return
+
+        link_infos = self.detect_inner_links(message.content)
+
+        if link_infos:
+            self.__remove_original_message(message)
+            new_link = self.__generate_new_link(link_infos)
+            print(f"New link: {new_link}")
+            return self.__create_new_message(message, link_infos, new_link)
+
+    def detect_inner_links(self, message: str = "") -> LinkInfos:
+        inner_link = re.match("https://(twitter.com|x.com)(/[a-zA-Z0-9_]{5,15}/status/[0-9]{0,20})", message)
+
+        if not inner_link:
+            # No link in the message -> we return false
+            return None
+
+        return LinkInfos(inner_link[0], inner_link[1], inner_link[2])
+
+    def __remove_original_message(self, message: discord.Message) -> bool:
+        try:
+            message.delete()
+        except discord.Forbidden:
+            print("Lacking the sufficient permissions to delete this message.")
+            return
+        except discord.NotFound:
+            print("The message couldn't be found. Maybe it was already deleted.")
+        except Exception:
+            print("Error when trying to delete this message.")
+
+    def __generate_new_link(self, link_infos: LinkInfos) -> str:
+        app_name = link_infos.app_name.strip(".com")
+        replacement = self.config.get("link_fix").get(app_name).get("replacement")
+
+        return "https://" + replacement + link_infos.post_id
+
+    def __create_new_message(self, message: discord.Message, link_infos: LinkInfos, new_link: str) -> str:
+        content = f"Lien {link_infos.app_name} détecté. Correction automatique.\n"
+        content += f"{new_link}\n"
+        content += f"Post original par @{message.author.display_name}\n"
+
+        message_other_content = re.sub(f"{link_infos.original_link}\\S*", "", message.content)
+        if message_other_content:
+            content += f"> {message_other_content}"
+        return content
