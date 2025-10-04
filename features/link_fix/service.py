f978f311e2aaddf3316020db8e2ff50a3ee3345e
Loloweb <lowgau@gmail.com>
2025-10-04 20:00:07 +0200

    handle multiple links maybe

--- features/link_fix/service.py
+++ features/link_fix/service.py
@@ -20,21 +20,29 @@ class LinkFixService:
         if not message:
             return
 
-        link_infos = self.detect_inner_links(message.content)
+        link_infos_list = self.detect_inner_links(message.content)
 
-        if link_infos:
-            new_link = self.__generate_new_link(link_infos)
-            print(f"New link: {new_link}")
-            return self.__create_new_message(message, link_infos, new_link)
+        if link_infos_list:
+            new_links = []
+            for link_infos in link_infos_list:
+                new_link = self.__generate_new_link(link_infos)
+                new_links.append(new_link)
+                print(f"New link: {new_link}")
+            
+            return self.__create_new_message(message, link_infos_list, new_links)
 
-    def detect_inner_links(self, message: str = "") -> LinkInfos:
-        inner_link = re.match("https://(twitter.com|x.com)(/[a-zA-Z0-9_]{5,15}/status/[0-9]{0,20})", message)
+    def detect_inner_links(self, message: str = "") -> list[LinkInfos]:
+        inner_links = re.finditer(r"https://(twitter.com|x.com)(/[a-zA-Z0-9_]{5,15}/status/[0-9]{0,20})", message)
 
-        if not inner_link:
+        if not inner_links:
             # No link in the message -> we return false
             return None
+        
+        link_infos_list = []
+        for match in inner_links:
+            link_infos_list.append(LinkInfos(match.group(0), match.group(1), match.group(2)))
 
-        return LinkInfos(inner_link[0], inner_link[1], inner_link[2])
+        return link_infos_list
 
     def __generate_new_link(self, link_infos: LinkInfos) -> str:
         app_name = link_infos.app_name.strip(".com")
@@ -42,12 +50,27 @@ class LinkFixService:
 
         return "https://" + replacement + link_infos.post_id
 
-    def __create_new_message(self, message: discord.Message, link_infos: LinkInfos, new_link: str) -> str:
-        content = f"Lien {link_infos.app_name} détecté. Correction automatique.\n"
-        content += f"{new_link}\n"
+    def __create_new_message(self, message: discord.Message, link_infos_list: list[LinkInfos], new_links: list[str]) -> str:
+        if len(link_infos_list) == 1:
+            # one link
+            link_infos = link_infos_list[0]
+            new_link = new_links[0]
+            content = f"Lien {link_infos.app_name} détecté. Correction automatique.\n"
+            content += f"{new_link}\n"
+        else:
+            # multiple links
+            content = f"{len(link_infos_list)} liens détectés. Correction automatique.\n"
+            
+            for new_link in new_links:
+                content += f"{new_link}\n"
+        
         content += f"Post original par {message.author.mention} ({message.author.display_name})\n"
 
-        message_other_content = re.sub(f"{link_infos.original_link}\\S*", "", message.content)
+        message_other_content = message.content
+        for link_infos in link_infos_list:
+            message_other_content = re.sub(f"{re.escape(link_infos.original_link)}\\S*", "", message_other_content)
+        
+        message_other_content = message_other_content.strip()
         if message_other_content:
             content += f"- {message_other_content}"
         return content
