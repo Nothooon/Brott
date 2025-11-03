8b767f679586d87d9738ee953a92c8a88eb3346f
Lolo <lowgau@gmail.com>
2025-11-03 13:18:57 +0100

    removed plural management(s?)
    Co-authored-by: Nothon :) <54975611+Nothooon@users.noreply.github.com>

--- features/link_fix/service.py
+++ features/link_fix/service.py
@@ -51,19 +51,9 @@ class LinkFixService:
         return "https://" + replacement + link_infos.post_id
 
     def __create_new_message(self, message: discord.Message, link_infos_list: list[LinkInfos], new_links: list[str]) -> str:
-        if len(link_infos_list) == 1:
-            # one link
-            link_infos = link_infos_list[0]
-            new_link = new_links[0]
-            content = f"Lien {link_infos.app_name} détecté. Correction automatique.\n"
-            content += f"{new_link}\n"
-        else:
-            # multiple links
-            content = f"{len(link_infos_list)} liens détectés. Correction automatique.\n"
-            
+            content = f"{len(link_infos_list)} lien(s) détecté(s). Correction automatique.\n"
             for new_link in new_links:
                 content += f"{new_link}\n"
-        
         content += f"Post original par {message.author.mention} ({message.author.display_name})\n"
 
         message_other_content = message.content
